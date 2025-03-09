ae292ff61d10d52599e14340c0418260da38f99d
ajay-jangid-github <ajaykumarjangid957@gmail.com>
2025-03-09 16:25:07 +0530

    initial commit

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,14 @@
+// const invoices = [
+//   {
+//     customer_id: customers[0].id,
+//     amount: 1000,
+//     status: 'pending',
+//     date: '2022-12-06',
+//   },
+//   {
+//     customer_id: customers[1].id,
+//     amount: 1334,
+//     status: 'pending',
+//     date: '2022-11-06',
+//   },
+// ];
